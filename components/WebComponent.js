6e8a2fbc25461ab6ca6dc1098b0653ff4c4c245a
Simeon Vincent <svincent@gmail.com>
2016-10-25 14:44:25 -0700

    Adds shorthand methods & cleans up docs

--- components/WebComponent.js
+++ components/WebComponent.js
@@ -10,15 +10,20 @@
 
 			// Bind the component's template to this instance
 			if (template) this.bindTemplate(template);
-
-			// Not sure if this is necessary -- commenting out for the moment
-			//
-			// this._styles = this._import.querySelector('link[rel="stylesheet"]')
-			// this._main.head.appendChild(_styles.cloneNode(true));
+			
+			// -----------------
+			// Shorthand Methods
+			// -----------------
+			
+			this.on = this.addEventListener;
 		}
+		
+		// -------
+		// Methods
+		// -------
 
 		logDebug (...args) {
-			if (this._debug) console.log.apply(console, args);
+			if (this._debug) console.log(...args);
 		}
 
 		listen (type, cb) {
@@ -28,10 +33,11 @@
 		bindTemplate (id) {
 			// Create an open component when in debug mode
 			const shadowOpts = {
-				mode: this._debug ? 'open' : 'closed'
+				mode: this._debug ? 'open' : 'closed';
 			};
 
-			// Implicitly creates `this.shadowRoot`, but that's inaccessible in closed mode
+			// `attachShadow` implicitly creates `this.shadowRoot`, but that's 
+			// inaccessible in closed mode.
 			this._root = this.attachShadow(shadowOpts);
 			const template = this._import.getElementById(id);
 			const instance = template.content.cloneNode(true);
@@ -39,8 +45,11 @@
 			this._root.appendChild(instance);
 			this.logDebug(`${this.constructor.name}.bindTemplate: Bound template to shadow root`);
 		}
+		
+		// -------------------
+		// Lifecycle callbacks
+		// -------------------
 
-		//
 		attributeChangedCallback (attrName, oldVal, newVal) {
 			this.logDebug('WebComponent:attributeChangedCallback');
 		}
@@ -58,6 +67,10 @@
 		}
 	}
 
+	// ----------------------
+	// WebComponent Utilities
+	// ----------------------
+	
 	// Finish the setup of a new WC. This includes registering the component with
 	// the DOM and setting static properties on the prototype required by the
 	// WebComponent constructor.
@@ -65,14 +78,13 @@
 	// * _doc - The document object that imported this WC
 	// * _import - The document that contains the template
 	WebComponent.setup = function setup (tag, Component) {
-			// # Cache references to our import script
-			//
-			// Cribbed from https://www.html5rocks.com/en/tutorials/webcomponents/imports/
+		// Cache references to our import script. Cribbed from
+		// https://www.html5rocks.com/en/tutorials/webcomponents/imports/
 		const proto = Component.prototype;
 		proto._doc = document;
 		proto._import = proto._doc.currentScript.ownerDocument;
 
-			// Register the element
+		// Register the element
 		customElements.define(tag, Component);
 	};
 
